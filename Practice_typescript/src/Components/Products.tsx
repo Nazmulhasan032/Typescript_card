ffa1f8361d80e7b1492a6f435b941b9a50b6b4e1
Nazmulhasan032 <nh30898@gmail.com>
2023-09-14 15:23:24 +0600

    Implement serch in card item with typescript

---
+++ Practice_typescript/src/Components/Products.tsx
@@ -0,0 +1,26 @@
+import { ProductType } from "../App";
+
+
+const Products = ({id, name, price, image}: ProductType) => {
+   
+    return (
+        <>
+            <div className="card w-96 bg-base-100 shadow-xl">
+                <figure><img src={image} alt="Shoes" /></figure>
+                <div className="card-body">
+                    <h2 className="card-title">
+                        {name}
+                        <div className="badge badge-secondary">NEW</div>
+                    </h2>
+                    <p>Price: {price}</p>
+                    <div className="card-actions justify-end">
+                        <div className="badge badge-outline">Fashion</div>
+                        <div className="badge badge-outline">Products</div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Products;
